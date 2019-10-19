eb0d2931076375d2bf6a2fe3323b594fb7aed17e
Dario <sardo.dario@gmail.com>
2019-10-19 19:06:01 +0200

    fix popup view model

--- ConfirmPopUp/ConfirmPopUp/View/PopUpOk.xaml.cs
+++ ConfirmPopUp/ConfirmPopUp/View/PopUpOk.xaml.cs
@@ -5,44 +5,14 @@ using Xamarin.Forms.Xaml;
 
 namespace ConfirmPopUp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PopUpOk : PopupPage
-	{
-        string _text;
-        string _title;
-        User _user;
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PopUpOk : PopupPage
+    {
 
-        public string Title
+        public PopUpOk(User user)
         {
-            get
-            {
-                return _title;
-            }
-            set
-            {
-                _title = value;
-                OnPropertyChanged();
-            }
-        }
-        public string Body
-        {
-            get
-            {
-                return _text;
-            }
-            set
-            {
-                _text = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public PopUpOk (User user)
-		{
-			InitializeComponent();
-            BindingContext = this;
-            Title = String.Format("Well done {0}!", user.Name);
-            Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+            InitializeComponent();
+            BindingContext = new PopUpPageViewModel(user);
         }
 
         public delegate void CallbackDelegate();
---
+++ ConfirmPopUp/ConfirmPopUp/ViewModel/PopUpPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfirmPopUp
+{
+    public class PopUpPageViewModel : BaseViewModel
+    {
+        string _text;
+        string _title;
+        User _user;
+
+        public PopUpPageViewModel(User user)
+        {
+            Title = String.Format("Well done {0}!", user.Name);
+            Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+        }
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Body
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
